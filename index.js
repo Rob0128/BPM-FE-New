4e08ff51bd9a87837748ce1b039954adb5fe63e1
rob hill <robertjohnhill1@gmail.com>
2024-06-30 11:01:12 +0100

    expo

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import 'react-native-gesture-handler';
 import {name as appName} from './app.json';
+import { registerRootComponent } from 'expo';
+import App from './App';
 
+registerRootComponent(App);
 AppRegistry.registerComponent(appName, () => App);
